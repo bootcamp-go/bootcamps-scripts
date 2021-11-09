4e7478f3c34e2d0d48371abf17ab428b280ed87a
extezgrosfe <ezekiel.grosfeld@digitalhouse.com>
2021-11-09 20:56:21 -0300

    add test after creation

--- meli_database.sh
+++ meli_database.sh
@@ -51,15 +51,21 @@ get_mysql_root_password() {
     echo
 }
 
+# Create user
+create_user() {
+    echo -e "${Green}Creating user...${NC}"
+    mysql -u root -p$1 -e "CREATE USER '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASS'"
+	mysql -u root -p$1 -e "GRANT ALL PRIVILEGES ON * . * TO '$MYSQL_USER'@'localhost'"
+	mysql -u root -p$1 -e "FLUSH PRIVILEGES"
+}
+
 # Use the mysql root password to create the database
 create_database() {
     get_mysql_root_password
     echo -e "${Blue}Creating database ${MYSQL_DATABASE}${NC}"
     mysql -u root -p$1 -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE"
     mysql -u root -p$1 $MYSQL_DATABASE < db.sql
-    mysql -u root -p$1 -e "CREATE USER '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASS'"
-	mysql -u root -p$1 -e "GRANT ALL PRIVILEGES ON * . * TO '$MYSQL_USER'@'localhost'"
-	mysql -u root -p$1 -e "FLUSH PRIVILEGES"
+    create_user $1
 }
 
 rebuild_database() {
@@ -71,12 +77,28 @@ rebuild_database() {
 	mysql -u root -p$1 -e "FLUSH PRIVILEGES"
 }
 
+test_database_creation() {
+    if [ -z "$(mysql -u root -p$1 -e "SHOW DATABASES LIKE '$MYSQL_DATABASE'" | grep $MYSQL_DATABASE)" ]; then
+        echo -e "${Red}Database $MYSQL_DATABASE was not created, retrying${NC}"
+        create_database $1
+    fi
+}
+
+test_user_creation() {
+    if [ -z "$(mysql -u root -p$1 -e "SELECT User FROM mysql.user WHERE User = '$MYSQL_USER'" | grep $MYSQL_USER)" ]; then
+        echo -e "${Red}User $MYSQL_USER was not created, retrying${NC}"
+        create_user $1
+    fi
+}
+
 main() {
     check_mysql_installed
     check_mysql_running
     # if argument is "create" then create the database and if its "rebuild" then rebuild the database
     if [ "$1" = "create" ]; then
         create_database $2
+        test_database_creation $2
+        test_user_creation $2
         echo -e "${Green}Database created successfully.${NC}"
     elif [ "$1" = "rebuild" ]; then
         rebuild_database $2
